b3bb7c634ca52a8d044d5df79f57dc55b2efa85d
Ubuntu <ubuntu@ip-172-31-23-65.us-west-2.compute.internal>
2013-07-17 03:44:15 +0000

    Modified web.js per assignment instructions attempt 1

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var buffer = fs.readFileSync('index.html');
+var indexstring = buffer.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexstring);
 });
 
 var port = process.env.PORT || 5000;
